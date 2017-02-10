cc8ddb575e97b99ced28f4e0338ef5622d3a3097
Erlend Thune <erlend.thune@gmail.com>
2017-02-10 15:08:08 +0100

    Første versjon.

---
+++ doit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ruby gethtmlfiles.rb
+ruby createvinodb.rb
+ruby createversionfile.rb
---
+++ startdocker.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+docker run -it  -v /Users/erlendthune/Desktop/ruby/polpriser:/polpriser ubuntubasic  /bin/bash
+
