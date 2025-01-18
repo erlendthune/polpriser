a134aac0d1d602372e2199c396fedf74fe5f8452
erlendthune <erlend.thune@gmail.com>
2025-01-18 21:00:41 +0100

    Delete files

--- doit.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ruby /polpriser/gethtmlfiles.rb
-ruby /polpriser/createvinodb.rb
-ruby /polpriser/createversionfile.rb
--- startdocker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker run -it  -v /Users/erlendthune/git/polpriser:/polpriser polpriser  /bin/bash
-
